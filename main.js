40971e562e4316bd6487e674f86604c14543f52f
Esen Esenaliev <eee.coool@gmail.com>
2017-11-01 21:23:14 +0600

    second commit

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
+var array = [];
 
-
-$('#price').click(function(){
+$('input[type=checkbox]').click(function(){
 	var sum = 0;
 	$('#price :checked').each(function(){
 		sum+=parseInt($(this).val());
 	});
 	$('#sum').text(sum);
+	if($(this).is(':checked')){
+		array.push($(this).parent().find('h3').html());
+	}
 });
 
+$('.btnsum').click(function(){
+	$.each(array, function(i,l){
+		$('.mdlul').append('<li>' + l + '</li>');
+	});
+});
+$('.delete').click(function(){
+	array = [];
 })
+
+});
